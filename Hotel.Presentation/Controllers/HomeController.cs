4e2a1b1b6c1f124712f3245218199fd7c0144af6
Mateusz Gębal <s10230mg@ms.wwsi.edu.pl>
2024-01-08 21:10:08 +0100

    dodanie ZapytanieODostepnosc, dodanie TypPokoju w Pokoj

--- Hotel.Presentation/Controllers/HomeController.cs
+++ Hotel.Presentation/Controllers/HomeController.cs
@@ -1,12 +1,22 @@
-﻿using Hotel.Presentation.Models;
+﻿using Hotel.Domain.Entities;
+using Hotel.Infrastructure.Presistence;
+using Hotel.Presentation.Models;
+using Hotel.Presentation.Models.Rezerwacja;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace Hotel.Presentation.Controllers
 {
     public class HomeController : Controller
     {
+        public HomeController(HotelDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         private readonly ILogger<HomeController> _logger;
+        private readonly HotelDbContext _dbContext;
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -27,6 +37,47 @@ namespace Hotel.Presentation.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public IActionResult ZapytanieODostepnosc()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ZapytanieODostepnosc(ZapytanieODostepnoscModel zapytanie)
+        {
+            if (zapytanie.DataOd == null || zapytanie.DataDo == null || zapytanie.IleOsob <= 0 || zapytanie.IleOsob == null)
+            {
+                return View();
+            }
+
+            if (zapytanie.DataOd >= zapytanie.DataDo)
+            {
+                return View();
+            }
+
+            var pokojeZajete = from p in _dbContext.Rezerwacje
+                               where
+                               ((zapytanie.DataOd >= p.DataOd) && (zapytanie.DataOd <= p.DataDo)) ||
+                               ((zapytanie.DataDo >= p.DataOd) && (zapytanie.DataDo <= p.DataDo)) ||
+                               ((zapytanie.DataOd <= p.DataDo) && (zapytanie.DataDo >= p.DataDo) && (zapytanie.DataDo <= p.DataDo)) ||
+                               ((zapytanie.DataOd >= p.DataDo) && (zapytanie.DataOd <= p.DataDo) && (zapytanie.DataDo >= p.DataDo)) ||
+                               ((zapytanie.DataOd <= p.DataDo) && (zapytanie.DataDo >= p.DataDo))
+                               select p;
+
+            var dostepnePokoje = _dbContext.Pokoje.Where(r => !pokojeZajete.Any(b => b.Id == r.Id)).ToList();
+
+            foreach (var pokoj in dostepnePokoje)
+            {
+                if (pokoj.LiczbaMiejsc >= zapytanie.IleOsob)
+                {
+                    zapytanie.ListaPokoi.Add(pokoj);
+                }
+            }
+
+            return View(zapytanie);
         }
     }
 }

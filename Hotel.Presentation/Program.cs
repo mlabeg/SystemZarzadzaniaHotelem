23abad2a90fd14ccc1ade4eabc16c1377306e3d1
Mateusz Gębal <s10230mg@ms.wwsi.edu.pl>
2024-01-20 21:20:17 +0100

    zmiana strony startowej update

--- Hotel.Presentation/Program.cs
+++ Hotel.Presentation/Program.cs
@@ -41,6 +41,4 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=PulpitRezerwacji}/{id?}");
 
-//pattern: "{controller=Home}/{action=Index}/{id?}");
-
 app.Run();

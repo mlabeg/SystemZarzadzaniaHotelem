60ffc62b261d8c3ffb7324655ce9ebfd2e747765
Mateusz Gębal <s10230mg@ms.wwsi.edu.pl>
2024-01-20 18:30:10 +0100

    dodanie kontrolera PulptRezerwacji

--- Hotel.Domain/Entities/Pokoj.cs
+++ Hotel.Domain/Entities/Pokoj.cs
@@ -6,46 +6,46 @@ using System.Threading.Tasks;
 
 namespace Hotel.Domain.Entities
 {
-    public class Pokoj
-    {
-        //nie wiem czy przez to nie trzeba będzie dodać nowej tabeli
+	public class Pokoj
+	{
+		//nie wiem czy przez to nie trzeba będzie dodać nowej tabeli
 
-        public enum PokojTyp
-        {
-            //https://www.hotelgdansk.com.pl/nasze-pokoje
+		public enum PokojTyp
+		{
+			//https://www.hotelgdansk.com.pl/nasze-pokoje
 
-            Double_Twin_Prestige,
-            Premium_Prestige,
-            Apartament_Prezydencki,
-            Twin_Business,
-            Apartament_Yachting
-        }
+			Double_Twin_Prestige,
+			Premium_Prestige,
+			Apartament_Prezydencki,
+			Twin_Business,
+			Apartament_Yachting
+		}
 
-        public PokojTyp? TypPokoju { get; set; }
+		public PokojTyp? TypPokoju { get; set; }
 
-        public int Id { get; set; }
-        public int Numer { get; set; }
-        public int LiczbaMiejsc { get; set; }
-        public int CenaZaNoc { get; set; }
-        public string Opis { get; set; }
+		public int Id { get; set; }
+		public int Numer { get; set; }
+		public int LiczbaMiejsc { get; set; }
+		public int CenaZaNoc { get; set; }
+		public string Opis { get; set; }
 
-        public bool CzyWolny { get; set; }//chyba do wywalenia
+		public bool CzyWolny { get; set; }//jak się usunie to nie działa BD
 
-        public Pokoj()
-        {
-            Opis = "";
-            CzyWolny = true;
-        }
+		public Pokoj()
+		{
+			Opis = "";
+			CzyWolny = true;
+		}
 
-        public Pokoj(int _numer, int _liczbaMiejsc)
-        {
-            Numer = _numer;
-            LiczbaMiejsc = _liczbaMiejsc;
-        }
+		public Pokoj(int _numer, int _liczbaMiejsc)
+		{
+			Numer = _numer;
+			LiczbaMiejsc = _liczbaMiejsc;
+		}
 
-        public Pokoj GetPokoj()
-        {
-            return this;
-        }
-    }
+		public Pokoj GetPokoj()
+		{
+			return this;
+		}
+	}
 }
--- Hotel.Domain/Entities/Rezerwacja.cs
+++ Hotel.Domain/Entities/Rezerwacja.cs
@@ -6,27 +6,28 @@ using System.Threading.Tasks;
 
 namespace Hotel.Domain.Entities
 {
-    public class Rezerwacja
-    {
-        public Rezerwacja()
-        {
-            Osoba = new UzytkownikNiezarejestrowany();
-            Status = "Oczekująca";
-        }
+	public class Rezerwacja
+	{
+		public Rezerwacja()
+		{
+			Osoba = new UzytkownikNiezarejestrowany();
+			Status = "Oczekująca";
+		}
 
-        public int Id { get; set; }
-        public DateTime DataOd { get; set; }
-        public DateTime DataDo { get; set; }
-        public int IloscOsob { get; set; }
-        public bool CzyZameldowano { get; set; } = false;
-        public bool CzyWymeldowano { get; set; } = false;
+		public int Id { get; set; }
+		public DateTime DataOd { get; set; }
+		public DateTime DataDo { get; set; }
+		public int IloscOsob { get; set; }
+		public bool CzyZameldowano { get; set; } = false;
+		public bool CzyWymeldowano { get; set; } = false;
 
-        public int CenaCalkowita { get; set; }
+		public int CenaCalkowita { get; set; }
 
-        public int PokojId { get; set; }
+		public int PokojId { get; set; }
+		public Pokoj Pokoj { get; set; }
 
-        public Osoba? Osoba { get; set; }
+		public Osoba? Osoba { get; set; }
 
-        public string Status { get; set; }//Oczekująca/W trakcie/Zakończona/Anulowana
-    }
+		public string Status { get; set; }//Oczekująca/W trakcie/Zakończona/Anulowana
+	}
 }

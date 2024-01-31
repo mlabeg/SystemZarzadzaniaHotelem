8b263d7d74e2db6973adf26592151b344ddf3831
Mateusz Gębal <s10230mg@ms.wwsi.edu.pl>
2024-01-31 23:59:01 +0100

    przeniesienie modeli z modułu Presentation do modułu Domain

--- Hotel.Presentation/Models/Rezerwacja/ZapytanieOSzczegolyRezerwacjiModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Hotel.Presentation.Models.Rezerwacja
-{
-	public class ZapytanieOSzczegolyRezerwacjiModel
-	{//TODO przenieść tą klasę do modułu Entities
-		public string AdresEmail { get; set; }
-		public int NumerRezerwacji { get; set; }
-	}
-}

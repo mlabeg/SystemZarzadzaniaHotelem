18912c076a664c5056e5f56fc43e92ca2860bffe
Mateusz Gębal <s10230mg@ms.wwsi.edu.pl>
2024-02-01 03:25:58 +0100

    usunięcie niepotrzebnych serwisów z modułu Application

--- Hotel.Application/Services/IRezerwacjaService.cs
+++ Hotel.Application/Services/IRezerwacjaService.cs
@@ -7,22 +7,22 @@ using System.Threading.Tasks;
 
 namespace Hotel.Application.Services
 {
-	public interface IRezerwacjaService
-	{
-		public Task DodajRezerwacje(Domain.Entities.Rezerwacja rezerwacja);
+    public interface IRezerwacjaService
+    {
+        public Task DodajRezerwacje(Domain.Entities.Rezerwacja rezerwacja);
 
-		public Task<bool> UsunRezerwacje(int id);
+        public Task<bool> DeleteReservation(int id);
 
-		public Task<IEnumerable<Domain.Entities.Rezerwacja>> ZwrocWszystkie(string? wybor);
+        public Task<IEnumerable<Domain.Entities.Rezerwacja>> ZwrocWszystkie(string? wybor);
 
-		public Task<Domain.Entities.Rezerwacja?> WyszukajPoId(int id);
+        public Task<Domain.Entities.Rezerwacja?> WyszukajPoId(int id);
 
-		public Task<IEnumerable<Domain.Entities.Rezerwacja>> ZwrocRezerwacjeWTermminie(DateTime dataOd, DateTime dataDo);
+        public Task<IEnumerable<Domain.Entities.Rezerwacja>> ZwrocRezerwacjeWTermminie(DateTime dataOd, DateTime dataDo);
 
-		public Task<List<int>>? WyszukajPokojIdWTermminie(DateTime dataOd, DateTime dataDo);
+        public Task<List<int>>? WyszukajPokojIdWTermminie(DateTime dataOd, DateTime dataDo);
 
-		//TODO dodać poniższe
-		//public List<Hotel.Domain.Entities.Rezerwacja> PokazHistorieRezerwacji();
-		//public List<Hotel.Domain.Entities.Rezerwacja> PokazAktualneRezerwacje();
-	}
+        //TODO dodać poniższe
+        //public List<Hotel.Domain.Entities.Rezerwacja> PokazHistorieRezerwacji();
+        //public List<Hotel.Domain.Entities.Rezerwacja> PokazAktualneRezerwacje();
+    }
 }
--- Hotel.Application/Services/RezerwacjaService.cs
+++ Hotel.Application/Services/RezerwacjaService.cs
@@ -10,45 +10,46 @@ using System.Threading.Tasks;
 
 namespace Hotel.Application.Services
 {
-	internal class RezerwacjaService : IRezerwacjaService
-	{
-		private readonly IRezerwacjeRepository _rezerwacjeRepository;
+    internal class RezerwacjaService : IRezerwacjaService
+    {
+        private readonly IRezerwacjeRepository _rezerwacjeRepository;
 
-		public RezerwacjaService(IRezerwacjeRepository rezerwacjeRepository)
-		{
-			_rezerwacjeRepository = rezerwacjeRepository;
-		}
+        public RezerwacjaService(IRezerwacjeRepository rezerwacjeRepository)
+        {
+            _rezerwacjeRepository = rezerwacjeRepository;
+            _anulujRezerwacje = anulujRezerwacje;
+        }
 
-		public async Task DodajRezerwacje(Domain.Entities.Rezerwacja rezerwacja)
-		{
-			await _rezerwacjeRepository.DodajRezerwacje(rezerwacja);
-		}
+        public async Task DodajRezerwacje(Domain.Entities.Rezerwacja rezerwacja)
+        {
+            await _rezerwacjeRepository.DodajRezerwacje(rezerwacja);
+        }
 
-		public async Task<bool> UsunRezerwacje(int id)
-		{
-			return await _rezerwacjeRepository.UsunRezerwacje(id);
-		}
+        public async Task<bool> DeleteReservation(int id)
+        {
+            return await _rezerwacjeRepository.UsunRezerwacje(id);
+        }
 
-		public async Task<IEnumerable<Domain.Entities.Rezerwacja>> ZwrocWszystkie(string? wybor)
-		{
-			var rezerwacje = await _rezerwacjeRepository.ZwrocWszystkie(wybor);
+        public async Task<IEnumerable<Domain.Entities.Rezerwacja>> ZwrocWszystkie(string? wybor)
+        {
+            var rezerwacje = await _rezerwacjeRepository.ZwrocWszystkie(wybor);
 
-			return rezerwacje;
-		}
+            return rezerwacje;
+        }
 
-		public async Task<Domain.Entities.Rezerwacja?> WyszukajPoId(int id)
-		{
-			return await _rezerwacjeRepository.WyszukajPoId(id);
-		}
+        public async Task<Domain.Entities.Rezerwacja?> WyszukajPoId(int id)
+        {
+            return await _rezerwacjeRepository.WyszukajPoId(id);
+        }
 
-		public async Task<IEnumerable<Domain.Entities.Rezerwacja>> ZwrocRezerwacjeWTermminie(DateTime dataOd, DateTime dataDo)
-		{
-			return await _rezerwacjeRepository.WyszukajWTermminie(dataOd, dataDo);
-		}
+        public async Task<IEnumerable<Domain.Entities.Rezerwacja>> ZwrocRezerwacjeWTermminie(DateTime dataOd, DateTime dataDo)
+        {
+            return await _rezerwacjeRepository.WyszukajWTermminie(dataOd, dataDo);
+        }
 
-		public async Task<List<int>>? WyszukajPokojIdWTermminie(DateTime dataOd, DateTime dataDo)
-		{
-			return await _rezerwacjeRepository.WyszukajPokojIdWTermminie(dataOd, dataDo);
-		}
-	}
+        public async Task<List<int>>? WyszukajPokojIdWTermminie(DateTime dataOd, DateTime dataDo)
+        {
+            return await _rezerwacjeRepository.WyszukajPokojIdWTermminie(dataOd, dataDo);
+        }
+    }
 }

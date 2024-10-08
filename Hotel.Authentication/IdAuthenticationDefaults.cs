243dfc9c136eb96b3c7ae9317fa8dd8b563a0ba6
Mateusz Gębal <s10230mg@ms.wwsi.edu.pl>
2024-10-08 19:59:41 +0200

    dodanie podstaw do systemu uwierzytelniania

---
+++ Hotel.Authentication/IdAuthenticationDefaults.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.Authentication
+{
+	public class IdAuthenticationDefaults
+	{
+		public const string AuthenticationScheme = "CookieAuthentication";
+		public const string CookieName = "IdCookie";
+	}
+}
